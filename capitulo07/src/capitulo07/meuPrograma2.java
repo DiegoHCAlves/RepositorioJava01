f943ce7b70777643456d3202cc7ae6ce08c7a264
Diego HC Alves <131674809+DiegoHCAlves@users.noreply.github.com>
2023-06-06 10:32:43 -0400

    Add files via upload

---
+++ capitulo07/src/capitulo07/meuPrograma2.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package capitulo07;
+
+/**
+ *
+ * @author ficdev
+ */
+public class meuPrograma2 {
+    public static void main(String[] args) {
+        Animal meusAnimais [] = new Animal[10];
+        meusAnimais[0] = new Animal(4,30);
+        meusAnimais[1] = new Cachorro(4,15,2);
+    }
+    
+}
